282b9dcf6d20d20ee1896a6b0c9f912d46b91360
Arthur669 <120607127+Arthur669@users.noreply.github.com>
2024-07-19 19:29:31 -0400

    Update main.py
    o codigo foi melhorado

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print("1- Atualizar o Windows")
-comandos = input("Digite o número do comando que preferir: ")
-if comandos == "1":
-    os.system("sfc /scannow")
+import subprocess
+while True:
+ print("1- Verificar e reparar arquivos do sistema")
+ comandos = input("Digite o número do comando que preferir: ")
+ if comandos == "1":
+    subprocess.run(["cmd", "sfc /scannow"])
