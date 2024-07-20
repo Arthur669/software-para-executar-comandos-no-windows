6eedb438fa57f6928e6b8b25b8bd17285ae31ad8
Arthur669 <120607127+Arthur669@users.noreply.github.com>
2024-07-20 19:25:15 -0400

    Grande atualização main.py
    Uma grande atualização que adicionou diversos outro comandos e deixou mais bonito o software.

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-import subprocess
+import os
 while True:
- print("1- Verificar e reparar arquivos do sistema")
+ print("1- Verificar e reparar arquivos do sistema 2-desligar o computador 3-reiniciar o computador")
+ print("4-sair 5- exibir informações sobre a configuração de rede do seu computador")
+ print("6- listar os processos em execução no seu computador")
+ print("-----------------------------------------------------------------------------------------")
  comandos = input("Digite o número do comando que preferir: ")
  if comandos == "1":
-    subprocess.run(["cmd", "sfc /scannow"])
+    os.system("sfc /scannow")
+    print("-----------------------------------------")
+ if comandos == "2":
+    os.system("shutdown /s")
+    print("-----------------------------------------")
+ if comandos == "3":
+    os.system("shutdown /r")
+    print("-----------------------------------------")
+ if comandos == "4":
+   exit()
+ print("-----------------------------------------")
+ if comandos == "5":
+    os.system("ipconfig")
+    print("-----------------------------------------")
+ if comandos == "6":
+    os.system("tasklist")
+    print("-----------------------------------------")
