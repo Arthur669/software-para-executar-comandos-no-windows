54b4878ef6c8003cd63fea6bf7b9560282cdfa0d
Arthur669 <120607127+Arthur669@users.noreply.github.com>
2024-07-21 17:08:15 -0400

    Nova atualização main.py
    Foi adicionado mais comandos ao software

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import os
 while True:
  print("1- Verificar e reparar arquivos do sistema 2-desligar o computador 3-reiniciar o computador")
  print("4-sair 5- exibir informações sobre a configuração de rede do seu computador")
- print("6- listar os processos em execução no seu computador")
+ print("6- listar os processos em execução no seu computador 7-limpar a tela")
+ print("8-mudar a cor das letras para verde 9-mudar a cor das letras para verde claro")
+ print("10-exibir informações detalhadas sobre o sistema operacional e o hardware do seu computador")
+ print("11-verificar e corrigir erros no disco rígido ou em uma unidade de armazenamento")
+ print("12-abrir o gerenciador de tarefas 13-exibir a versão do Windows instalado no computador")
  print("-----------------------------------------------------------------------------------------")
  comandos = input("Digite o número do comando que preferir: ")
  if comandos == "1":
@@ -22,4 +26,25 @@ while True:
     print("-----------------------------------------")
  if comandos == "6":
     os.system("tasklist")
+    print("-----------------------------------------")
+ if comandos == "7":
+    os.system("cls")
+    print("-----------------------------------------")
+ if comandos == "8":
+    os.system("color 2")
+    print("-----------------------------------------")
+ if comandos == "9":
+    os.system("color a")
+    print("-----------------------------------------")
+ if comandos == "10":
+    os.system("systeminfo")
+    print("-----------------------------------------")
+ if comandos == "11":
+    os.system("chkdsk")
+    print("-----------------------------------------")
+ if comandos == "12":
+    os.system("taskmgr")
+    print("-----------------------------------------")
+ if comandos == "13":
+    os.system("ver")
     print("-----------------------------------------")
